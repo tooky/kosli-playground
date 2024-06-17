694e6ff7729fd3b41bfc1d6ef7d7c0e4545c681c
JonJagger <jon@kosli.com>
2024-06-17 15:31:10 +0100

    Ensure run.sh scripts work when run from any cwd

--- beta/run.sh
+++ beta/run.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -Eeu
 
-# TODO: ensure this can be run from any directory
+readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+pushd "${MY_DIR}" &> /dev/null
+trap "popd &> /dev/null" INT EXIT
+
 docker build . -t playground/beta
 docker run -p4501:4501 playground/beta
