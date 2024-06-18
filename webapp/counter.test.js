d31a037cf4ad48d57cb1bb28f45cc2ebacf24abc
Steve Tooke <steve@took.es>
2024-06-18 12:02:46 +0100

    Add webapp test and coverage

---
+++ webapp/counter.test.js
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest'
+import { setupCounter } from './counter'
+
+test('it works', () => {
+   expect(true).toEqual(true)
+})
--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -2,5 +2,9 @@ export default {
   server: {
     host: true,
     port: 4502
-  }
+  },
+  test: {
+    reporters: ['junit', 'default'],
+    outputFile: './junit-report.xml'
+  },
 }
