23283cd046b72a9bf50539dda9b35e93aec1de0e
JonJagger <jon@kosli.com>
2024-06-18 12:53:05 +0100

    Delete dead script

--- beta/sh/run_tests_with_coverage.sh
+++ beta/sh/run_tests_with_coverage.sh
@@ -4,7 +4,6 @@ set -Eeu
 export ROOT_DIR="$(git rev-parse --show-toplevel)/beta"
 export SH_DIR="${ROOT_DIR}/sh"
 
-source "${SH_DIR}/containers_down.sh"
 source "${SH_DIR}/containers_up_healthy_and_clean.sh"
 source "${SH_DIR}/test_in_containers.sh"
 source "${SH_DIR}/echo_env_vars.sh"
@@ -13,7 +12,6 @@ export $(echo_env_vars)
 run_tests_with_coverage()
 {
   exit_code=0
-  containers_down
   server_up_healthy_and_clean
   test_in_containers "$@" || exit_code=$?
   return ${exit_code}
