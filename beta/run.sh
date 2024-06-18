e620a958e422c437b242986255999de8327ce4e7
JonJagger <jon@kosli.com>
2024-06-18 10:23:20 +0100

    Delete dead file

--- beta/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -Eeu
-
-readonly MY_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-pushd "${MY_DIR}" &> /dev/null
-trap "popd &> /dev/null" INT EXIT
-
-docker build . --tag playground/beta
-docker run -p4501:4501 playground/beta
