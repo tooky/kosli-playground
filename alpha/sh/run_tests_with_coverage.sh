3e291389f6366c83d33cfcdefbad856acc441cc0
JonJagger <jon@kosli.com>
2024-06-18 12:56:45 +0100

    Simplify bash helper function

--- alpha/sh/run_tests_with_coverage.sh
+++ alpha/sh/run_tests_with_coverage.sh
@@ -7,14 +7,13 @@ export SH_DIR="${ROOT_DIR}/sh"
 source "${SH_DIR}/containers_up_healthy_and_clean.sh"
 source "${SH_DIR}/test_in_containers.sh"
 source "${SH_DIR}/echo_env_vars.sh"
+# shellcheck disable=SC2046
 export $(echo_env_vars)
 
 run_tests_with_coverage()
 {
-  exit_code=0
   server_up_healthy_and_clean
-  test_in_containers "$@" || exit_code=$?
-  return ${exit_code}
+  test_in_containers "$@"
 }
 
 if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
