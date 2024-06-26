b8d8781a6a2e1fb171615ffbb01b4cc5d287fdf3
JonJagger <jon@kosli.com>
2024-06-26 10:03:06 +0100

    Rename the main script file

--- run_ci_locally.sh
+++ run_ci_locally.sh
@@ -4,7 +4,19 @@ set -Eeu
 # Script to run github CI pipelines locally using act
 # https://github.com/nektos/act
 
+# INSTALL DOCKER
+# --------------
+# I'm assuming docker is already installed.
+
+# INSTALL ACT
+# -----------
 # $ brew install act
+
+# If you install using the bash command provided at https://nektosact.com/installation/index.html#bash-script
+#   curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/nektos/act/master/install.sh | sudo bash
+# Then to uninstall act on a MacBook I had to do this:
+# rm -rf "~/Library/Application Support/act"
+
 # When running act for the first time, you have a choice of "level"
 #   - Large: supports a lot of github actions, a massive image
 #   - Medium: support the most common github actions, much smaller image
@@ -12,7 +24,8 @@ set -Eeu
 # Small does not work for the actions we have in the CI pipeline (eg docker/build-push-action)
 # Medium does work for all the actions we need, so select that to get started faster.
 
-# Install gh
+# INSTALL GH
+# ----------
 # Needed for the $(gh auth token) below
 # The token needs permission to push to the ghcr registry.
 # This is not in the default set of token scopes.
@@ -30,11 +43,39 @@ set -Eeu
 #   click [continue] button
 #   click [authorize github] button
 
-./bin/act \
+exit_non_zero_unless_installed()
+{
+  for dependent in "$@"
+  do
+    if ! installed "${dependent}" ; then
+      stderr "${dependent}" is not installed
+      exit 42
+    fi
+  done
+}
+
+installed()
+{
+  local -r dependent="${1}"
+  echo -n "Checking: is ${dependent} installed? "
+  if hash "${dependent}" 2> /dev/null; then
+    echo "yes"
+    true
+  else
+    echo "NO"
+    false
+  fi
+}
+
+stderr()
+{
+  local -r message="${1}"
+  >&2 echo "ERROR: ${message}"
+}
+
+exit_non_zero_unless_installed docker act gh
+
+act \
   --secret=GITHUB_TOKEN="$(gh auth token)" \
   --secret-file=.act.secrets \
   --var-file=.act.variables
-
-
-# To uninstall act on MacBook I had to
-# rm -rf "~/Library/Application Support/act"
