8e83e17441e82ea1b320573b60b1473224a7e0a6
JonJagger <jon@kosli.com>
2024-06-26 02:42:12 +0100

    Try act for running ci locally

---
+++ run_act.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -Eeu
+
+# gh auth login --scopes=write:packages
+#  > GitHub.com
+#  > I want to re-authenticate   (y)
+#  > HTTPS
+#  > Authenticate Git with your GitHub credentials? (Y)
+#  > Login with a web browser
+#  copy the code into the browser page
+#  hit return
+#  enter the code in the web-browser
+#  click [continue] button
+#  click [authorize github] button
+
+./bin/act \
+  --secret=GITHUB_TOKEN="$(gh auth token)" \
+  --secret-file=.act.secrets
